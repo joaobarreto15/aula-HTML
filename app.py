388d611cc2a45c44f9e5966801cf32a6a2aa73f6
joaobarreto15 <barretomagalhaesjp@gmail.com>
2024-10-25 15:17:09 -0300

    lista de exercicio

--- app.py
+++ app.py
@@ -53,6 +53,48 @@ def tabuada(numero = None):
          
   return render_template('tabuada.html', numero=numero)
 
+@app.route("/juros")
+@app.route("/juros/<numero>", methods=("GET", ))
+def juros(juros = None , capital= None , tempo= None , deposito= None):
+   
+  if 'juros'and 'capital' and 'tempo' and 'deposito' in request.args: 
+    
+    juros = int(request.args.get('juros'))
+    capital = int(request.args.get('juros'))
+    tempo = int(request.args.get('juros'))
+    deposito = int(request.args.get('juros'))
+    
+         
+  return render_template('juros.html', juros=juros , capital=capital , tempo=tempo , deposito=deposito)
+
+
+@app.route("/login", methods=("GET", ))
+def login(email = None , senha= None ):
+   
+  if 'email'and 'senha' in request.args: 
+    
+    email = int(request.args.get('email'))
+    senha = int(request.args.get('senha'))
+   
+    
+         
+  return render_template('login.html', email=email , senha=senha)
+
+@app.route("/imc", methods=("GET",))
+def imc(peso = None, altura= None):
+  if "peso" and "altura" in request.args:
+    peso =float(request.args.get('peso')) 
+    altura=float(request.args.get('altura'))
+  return render_template("imc.html", peso=peso, altura=altura)
+
+
+
+
+
+
+
+   
+ 
 
 
 
